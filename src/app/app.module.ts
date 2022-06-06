f2cf2fabed67fe9a408510fb546410db167f31d9
eskokado <eskokado@gmail.com>
2022-06-06 03:36:45 -0300

    fix: folder directive

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,7 +4,7 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { MarcaTextoDirective } from './directives/marca-texto.directive';
+import { MarcaTextoDirective } from './shared/directives/marca-texto.directive';
 
 @NgModule({
   declarations: [
